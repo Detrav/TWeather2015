1d056ec5c039b837300d7068343425adc36a8cae
Detrav <witalyezep@gmail.com>
2015-12-26 11:30:03 +0300

    тесты на десктоп чайлд

--- TWeather2015/MainWindow.xaml.cs
+++ TWeather2015/MainWindow.xaml.cs
@@ -27,20 +27,20 @@ namespace TWeather2015
         public MainWindow()
         {
             InitializeComponent();
-            SetBottom();
+
         }
 
         protected override void OnSourceInitialized(EventArgs e)
         {
-            base.OnSourceInitialized(e);
-            HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
-            source.AddHook(WndProc);
+            /* base.OnSourceInitialized(e);
+             HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
+             source.AddHook(WndProc);*/
         }
 
         private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
-            
-            switch(msg)
+
+            switch (msg)
             {
                 case WM_ACTIVATE:
                     SetBottom();
@@ -89,5 +89,25 @@ namespace TWeather2015
             if (WindowState != WindowState.Normal)
                 WindowState = WindowState.Normal;
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (handle == IntPtr.Zero) handle = new WindowInteropHelper(this).Handle;
+            IntPtr hwndf = handle;
+            IntPtr hwndParent = FindWindow("ProgMan", null);
+            SetParent(hwndf, hwndParent);
+            this.Topmost = false;
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr FindWindow(
+  [MarshalAs(UnmanagedType.LPTStr)] string lpClassName,
+  [MarshalAs(UnmanagedType.LPTStr)] string lpWindowName);
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetParent(
+          IntPtr hWndChild,      // handle to window
+          IntPtr hWndNewParent   // new parent window
+          );
     }
 }
