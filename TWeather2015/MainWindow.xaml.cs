87c77fb2cb272d8d0d7f777bcc5b4b063792b89b
Detrav <witalyezep@gmail.com>
2015-12-28 13:58:29 +0300

    Чистка, большое окно внутри шела, файл вотчеры

--- TWeather2015/MainWindow.xaml.cs
+++ TWeather2015/MainWindow.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.Windows.Threading;
+using System.IO;
+
 
 namespace TWeather2015
 {
@@ -34,100 +24,93 @@ namespace TWeather2015
         3-3.    Выключаемся.
         */
         IntPtr handle = IntPtr.Zero;
-        TestWindow tw = new TestWindow();
+        IntPtr desktopHandle = IntPtr.Zero;
+
+        FileSystemWatcher fswDesktop = new FileSystemWatcher(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory));
+        FileSystemWatcher fswCommonDesktop = new FileSystemWatcher(Environment.GetFolderPath(Environment.SpecialFolder.CommonDesktopDirectory));
+
         public MainWindow()
         {
             InitializeComponent();
-            tw.Show();
+            fswDesktop.Changed += FswDesktop_Changed;
+            fswDesktop.Created += FswDesktop_Changed;
+            fswDesktop.Deleted += FswDesktop_Changed;
+            fswDesktop.Renamed += FswDesktop_Changed;
+
+            fswCommonDesktop.Changed += FswDesktop_Changed;
+            fswCommonDesktop.Created += FswDesktop_Changed;
+            fswCommonDesktop.Deleted += FswDesktop_Changed;
+            fswCommonDesktop.Renamed += FswDesktop_Changed;
+
+            fswDesktop.EnableRaisingEvents = true;
+            fswCommonDesktop.EnableRaisingEvents = true;
         }
 
-        protected override void OnSourceInitialized(EventArgs e)
-        {
-            /* base.OnSourceInitialized(e);
-             HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
-             source.AddHook(WndProc);*/
-        }
-
-        
-
-
-        public void SetBottom(IntPtr after)
-        {
-            if (handle == IntPtr.Zero) handle = new WindowInteropHelper(this).Handle;
-            SetWindowPos(handle, after, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
-            Console.WriteLine("SetButtom after {0}", after);
-        }
-
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr FindWindow([MarshalAs(UnmanagedType.LPTStr)] string lpClassName, [MarshalAs(UnmanagedType.LPTStr)] string lpWindowName);
         [DllImport("user32.dll")]
-        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+        public static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
-        const UInt32 SWP_NOSIZE = 0x0001;
-        const UInt32 SWP_NOMOVE = 0x0002;
-        const UInt32 SWP_NOACTIVATE = 0x0010;
-        const UInt32 SWP_NOZORDER = 0x0004;
-        const int WM_WINDOWPOSCHANGING = 0x0046;
-        const int WM_ACTIVATE = 0x0006;
-        const int WM_MOVE = 0x0003;
 
-        static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
-        [StructLayout(LayoutKind.Sequential)]
-        public struct WINDOWPOS
-        {
-            public IntPtr hwnd;
-            public IntPtr hwndInsertAfter;
-            public int x;
-            public int y;
-            public int cx;
-            public int cy;
-            public uint flags;
-        }
 
         private void Window_StateChanged(object sender, EventArgs e)
         {
-            if (WindowState != WindowState.Normal)
-                WindowState = WindowState.Normal;
+            if (WindowState != WindowState.Maximized)
+                WindowState = WindowState.Maximized;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             if (handle == IntPtr.Zero) handle = new WindowInteropHelper(this).Handle;
-            IntPtr hwndf = handle;
+
             IntPtr hwndParent = FindWindow("ProgMan", null);
             IntPtr next = FindWindowEx(hwndParent, IntPtr.Zero, "SHELLDLL_DefView", null);
-            SetParent(tw.handle(), next);
-            SetParent(hwndf, next);
-            next = FindWindowEx(next, IntPtr.Zero, "SysListView32", "FolderView");
+            SetParent(handle, next);
+            desktopHandle = FindWindowEx(next, IntPtr.Zero, "SysListView32", "FolderView");
+            if(desktopHandle!=IntPtr.Zero)
             {
-                var i = GetWindowLong(next, -16);
-                if ((i & 0x04000000L) > 0) i -= 0x04000000;
-                Console.WriteLine(i);
-                SetWindowLong(next, -16, i);
+                ShowWindow(desktopHandle, 0);
+                //Убираем сущ
             }
-            SetParent(next, hwndf);
-            //ShowWindow(next, 1);
-            //SetBottom(next);
             this.Topmost = true;
+            WindowState = WindowState.Maximized;
         }
 
-        [DllImport("User32")]
-        private static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (desktopHandle != IntPtr.Zero)
+            {
+                ShowWindow(desktopHandle, 1);
+                //Возвращаем
+            }
+        }
 
-        [DllImport("user32.dll")]
-        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-        [DllImport("user32.dll")]
-        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
 
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            //Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory));
+            //Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.CommonDesktopDirectory));
+            if (e.Data.GetDataPresent(DataFormats.FileDrop) == true)
+            {
+                string[] FileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+                foreach (string File in FileList)
+                    Console.WriteLine(File);
+            }
+        }
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern IntPtr FindWindow(
-  [MarshalAs(UnmanagedType.LPTStr)] string lpClassName,
-  [MarshalAs(UnmanagedType.LPTStr)] string lpWindowName);
-        [DllImport("user32.dll")]
-        public static extern IntPtr SetParent(
-          IntPtr hWndChild,      // handle to window
-          IntPtr hWndNewParent   // new parent window
-          );
+        private void FswDesktop_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine(e.ChangeType);
+            //throw new NotImplementedException();
+        }
     }
 }
